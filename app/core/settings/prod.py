b3f7e3e80a9c020280d0c1a5e72609d8fca180e9
kemickoko <kemickokojiji.work@gmail.com>
2025-08-06 22:49:20 +0900

    S3に上がらない理由解析中

--- app/core/settings/prod.py
+++ app/core/settings/prod.py
@@ -40,8 +40,8 @@ INSTALLED_APPS += ['storages']
 AWS_STORAGE_BUCKET_NAME = 'iterms-static'
 AWS_S3_REGION_NAME = 'ap-northeast-1'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app', 'static')]
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_CLOUDFRONT_DOMAIN = "d3t658gdoc1u83.cloudfront.net"

fa0d5c19a4c13c729e8fcde955ebc28f47f12d80
kemickoko <kemickokojiji.work@gmail.com>
2025-08-06 22:32:50 +0900

    試しに

--- app/core/settings/prod.py
+++ app/core/settings/prod.py
@@ -46,3 +46,6 @@ STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_CLOUDFRONT_DOMAIN = "d3t658gdoc1u83.cloudfront.net"
 STATIC_URL = f"https://{AWS_CLOUDFRONT_DOMAIN}/"
+
+if 'STATIC_ROOT' in globals():
+    del STATIC_ROOT

0543b8f117da073cdb415a174e04e67d89f49ad6
kemickoko <kemickokojiji.work@gmail.com>
2025-08-16 14:25:14 +0900

    DEBUG=Falseに修正

--- app/core/settings/prod.py
+++ app/core/settings/prod.py
@@ -5,7 +5,7 @@ debug_env = os.getenv("DEBUG", "False").lower()
 if debug_env in ("true", "1", "yes"):
     raise RuntimeError("DEBUG must be False in production!")
 
-DEBUG = True
+DEBUG =False
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 

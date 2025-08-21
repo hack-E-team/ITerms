532910c5a7e35c03f4eebc4b9dfc7964f0583b49
s0s0g1 <passione0716@gmail.com>
2025-08-21 23:14:01 +0900

    login画面編集

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -5,9 +5,6 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dummy_dashboard_view, name='dashboard'),
-]
-
-urlpatterns = [
     path('summary', views.summary, name='summary'),          # ?days=30
     path('recent', views.recent, name='recent'),             # ?days=30&limit=20&offset=0
     path('vocabs', views.vocabs, name='vocabs'),             # ?days=90

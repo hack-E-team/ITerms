e9437442cfacdecdecd1a785d4fb4c907772808e
kouta49 <kouta20051126@icloud.com>
2025-08-26 17:19:41 +0900

    feat(dashboard): add home route; update requirements

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -8,6 +8,7 @@ app_name = 'dashboard'
 # ]
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path('summary', views.summary, name='summary'),          # ?days=30
     path('recent', views.recent, name='recent'),             # ?days=30&limit=20&offset=0
     path('vocabs', views.vocabs, name='vocabs'),             # ?days=90

df0ac7e63f32b2cc94f06427c5685719a530af46
kemickoko <kemickokojiji.work@gmail.com>
2025-08-27 16:57:58 +0900

    用語編集、用語帳編集、その他微修正

--- app/terms/urls.py
+++ app/terms/urls.py
@@ -5,11 +5,15 @@ from . import views
 app_name = "terms"
 
 urlpatterns = [
-    path("", views.terms_list_view, name="list"),
+    path("list/", views.terms_list_view, name="list"),
 
     # 用語作成
-    path("create/", views.term_create_view, name="createterms"),
+    path("myterms/", views.term_create_view, name="createterms"),
     path("create/submit/", views.term_create_post, name="create_post"),
+    
+    
+    path("<int:pk>/edit/", views.term_edit_view, name="edit"),
+    path("<int:pk>/edit/submit/", views.term_edit_post, name="edit_post"),
 
     # JSON API
     path("api/flashcards/", views.terms_api_flashcards, name="api_flashcards"),
